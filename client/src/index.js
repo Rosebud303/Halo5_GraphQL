a582191bbdc7dfbb292acebbf83c4e9de3606e4a
Jeremy Poulter <Jpoulter@gmail.com>
2020-05-09 17:50:25 -0600

    Finish index setup using store and reducers

--- client/src/index.js
+++ client/src/index.js
@@ -10,6 +10,7 @@ import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+import rootReduceer from './reducers';
 
 const store = createStore(rootReduceer, composeWithDevTools(applyMiddleware(thunk)))
 
