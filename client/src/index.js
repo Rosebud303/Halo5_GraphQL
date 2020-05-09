1a7ba41aa36f68993b4def8bb1290070d4c33a44
Jeremy Poulter <Jpoulter@gmail.com>
2020-05-09 17:37:33 -0600

    Setup redux and router

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App/App';
 import * as serviceWorker from './serviceWorker';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+
+const store = createStore(rootReduceer, composeWithDevTools(applyMiddleware(thunk)))
+
 const client = new ApolloClient({
   uri: 'http://localhost:5000/graphql'
 });
-ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
-  document.getElementById('root')
-);
+
+const router = (
+  <Router>
+      <Provider store={store}>
+        <ApolloProvider client={client}>
+          <App />
+        </ApolloProvider>
+      </Provider>
+  </Router>
+) 
+ReactDOM.render(router, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
