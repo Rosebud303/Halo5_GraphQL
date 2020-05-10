298f783c46d2cc7ac44822dfdd0ab052d3370a25
Jeremy Poulter <Jpoulter@gmail.com>
2020-05-10 14:59:13 -0600

    Create container directories and functional components for next 3 pages

---
+++ client/src/containers/Arenapage/Arenapage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Arenapage.scss';
+
+const Arenapage = () => {
+  return (
+    <div>
+      <h1>Arena Page Coming Soon...</h1>
+    </div>
+  )
+}
+
+export default Arenapage;
