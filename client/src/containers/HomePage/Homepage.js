4d13bedb35cf5848ac499f8fa137d08838a3c068
Jeremy Poulter <Jpoulter@gmail.com>
2020-05-10 15:47:00 -0600

    Create test sprites for medals

--- client/src/containers/HomePage/Homepage.js
+++ client/src/containers/HomePage/Homepage.js
@@ -71,6 +71,16 @@ class Homepage extends Component {
             />
             <button className='welcome-search welcome-search-button' type='submit'>Submit</button>
           </form>
+          {/* *********************************DELETE LATER *******************************/}
+          <div className='test-sprites-container'>
+            <div className='test-sprites1'></div>
+            <div className='test-sprites2'></div>
+            <div className='test-sprites3'></div>
+            <div className='test-sprites4'></div>
+            <div className='test-sprites5'></div>
+            <div className='test-sprites6'></div>
+          </div>
+          {/* *********************************DELETE LATER *******************************/}
       </div>
       <div className='lesser-spartan-details'>
         <section className='spartan-gfx'>
