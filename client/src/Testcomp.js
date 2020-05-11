240604046a79f81addd8b530649f5f40d614ee32
Jeremy Poulter <Jpoulter@gmail.com>
2020-05-11 16:26:42 -0600

    Create iteration of metadata queries to upload to localStorage
    Co-authored-by: Jeremy Poulter <jpoulter@gmail.com>
    Co-authored-by: Duy Vu <duyvthanh@gmail.com>

--- client/src/Testcomp.js
+++ client/src/Testcomp.js
@@ -2,40 +2,31 @@ import React from 'react';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 import Spinner from './Spinner/Spinner';
-import { CSR_QUERY } from './MetadataQueries/MetadataQueries';
+import { metadataQueryList } from './MetadataQueries/MetadataQueries';
 
-
-
-
-const MEDAL_QUERY = gql`
-  query MedalQuery($player_name: String!){
-    spartan(player_name: $player_name){
-      Gamertag
-      ServiceTag
-    }
-  }
-`;
-
-let player_name = "daymanf0tnm"
 
 export default function Testcomp() {
 
-
   return (
     <div>
-      <Query query={CSR_QUERY}>
-        {
-          (
-            { loading, error, data }) => {
-              if(loading) return <p>loading...</p>
-              if(error) console.log(error)
-              console.log(data)
-              localStorage.setItem("csrMetadata", JSON.stringify(data))
-              return <p>test</p>
-          }
-        }
-      </Query>
-      
+      {
+        metadataQueryList.map(metadata => {
+          return localStorage[metadata.name] ? 
+            <p>{metadata.name} already exists in local storage</p>
+            : 
+            <Query query={metadata.query}>
+              {
+                ({ loading, error, data }) => {
+                  if(loading) return <p>loading...</p>
+                  if(error) console.log(error)
+                  localStorage.setItem(`${metadata.name}`, JSON.stringify(data))
+                  return <p>testtttt</p>
+                } 
+              }
+            </Query>
+
+        })
+      }
     </div>
   )
 }
