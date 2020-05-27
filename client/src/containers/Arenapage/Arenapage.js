c6b2d6ac30dbebb85df326730f9d9255aa4425f2
Jeremy Poulter <Jpoulter@gmail.com>
2020-05-27 15:48:51 -0600

    Move kda content into shared box and styled kills to kda box

--- client/src/containers/Arenapage/Arenapage.js
+++ client/src/containers/Arenapage/Arenapage.js
@@ -60,6 +60,21 @@ let SELECTED_VARIANT_QUERY = gql`
   }
 `;
 
+let ARENA_CSR_QUERY = gql`
+  query CsrQuery($player_name: String!) {
+    arenaCsr(player_name: $player_name) {
+      HighestCsrAttained {
+        Tier
+        DesignationId
+        PercentToNextTier
+      }
+      HighestCsrPlaylistId
+      HighestCsrSeasonId
+      ArenaPlaylistStatsSeasonId
+    }
+  }
+`
+
 class Arenapage extends Component {
   constructor(props) {
     super(props);
@@ -91,6 +106,8 @@ class Arenapage extends Component {
     const GameBaseVariantId = this.state.currentGameVariant;
     const parsedWeaponsMetadata = JSON.parse(localStorage.getItem("weaponsMetadata"));
     const parsedMedalsMetadata = JSON.parse(localStorage.getItem("medalsMetadata"));
+    const parsedCsrMetadata = JSON.parse(localStorage.getItem("csrMetadata"));
+    const parsedSeasonsMetadata = JSON.parse(localStorage.getItem("seasonsMetadata"));
 
     return (
       <div className='arena-page'>
@@ -183,25 +200,44 @@ class Arenapage extends Component {
                 <div className='arena-content-container'>
                   <main className='arena-main'>
                     <div className='arena-details-box box-a'>
-                      <h4 className='box-title'>Win Rate - {(TotalGamesWon / TotalGamesCompleted * 100).toFixed(2)}%</h4>
-                      <div>
-                        <p className='box-details'>Wins: {TotalGamesWon.toLocaleString()}</p>
-                        <p className='box-details'>Losses: {TotalGamesLost.toLocaleString()}</p>
-                        <p className='box-details'>Ties: {TotalGamesTied.toLocaleString()}</p>
+                      <div className='wins-container shared-container'>
+                        <h4 className='box-title'>Win Rate {(TotalGamesWon / TotalGamesCompleted * 100).toFixed(2)}%</h4>
+                        <div>
+                          <p className='box-details'>Wins: {TotalGamesWon.toLocaleString()}</p>
+                          <p className='box-details'>Losses: {TotalGamesLost.toLocaleString()}</p>
+                          <p className='box-details'>Ties: {TotalGamesTied.toLocaleString()}</p>
+                        </div>
+                      </div>
+                      <div className='kda-container shared-container'>
+                        <h4 className='box-title'>KDA {((TotalKills + TotalAssists / 3) / TotalDeaths).toFixed(3)}</h4>
+                        <div>
+                          <p className='box-details'>Kills: {TotalKills.toLocaleString()}</p>
+                          <p className='box-details'>Deaths: {TotalDeaths.toLocaleString()}</p>
+                          <p className='box-details'>Assists: {TotalAssists.toLocaleString()}</p>
+                        </div>
                       </div>
                     </div>
                     <div className='arena-details-box box-b'>
-                      <h4 className='box-title'>KDA - {((TotalKills + TotalAssists / 3) / TotalDeaths).toFixed(3)}</h4>
-                      <p className='box-details'>Kills: {TotalKills.toLocaleString()}</p>
-                      <p className='box-details'>Deaths: {TotalDeaths.toLocaleString()}</p>
-                      <p className='box-details'>Assists: {TotalAssists.toLocaleString()}</p>
+                      <h4 className='box-title'>Style Kills</h4>
+                      <div className='kills-details-container'>
+                        <div className='kills-details-column-container'>
+                          <p className='box-details'>Assassinations: {TotalAssassinations.toLocaleString()}</p>
+                          <p className='box-details'>Melee: {TotalMeleeKills.toLocaleString()}</p>
+                          <p className='box-details'>Grenade: {TotalGrenadeKills.toLocaleString()}</p>
+                        </div>
+                        <div>
+                          <p className='box-details'>Power Weapon: {TotalPowerWeaponKills.toLocaleString()}</p>
+                          <p className='box-details'>Ground Pound: {TotalGroundPoundKills.toLocaleString()}</p>
+                          <p className='box-details'>Shoulder Bash: {TotalShoulderBashKills.toLocaleString()}</p>
+                        </div>
+                      </div>
                     </div>
                     <div className='arena-details-box box-c weapon-container'>
                       <h4 className='box-title'>Best Performing Weapon</h4>
                       <p className='hover-instructions'>(hover for details)</p>
                       <img className='weapon-image' src={foundWeapon.largeIconImageUrl} alt='Weapon' />
                       <div className='best-wep-info'>
-                        <p><strong><u>{foundWeapon.name}</u></strong></p>
+                        <p className='best-wep-title'>{foundWeapon.name}</p>
                         <p className='box-details'>Kills: {WeaponWithMostKills.TotalKills.toLocaleString()}</p>
                         <p className='box-details'>Damage Dealt: {parseInt(WeaponWithMostKills.TotalDamageDealt).toLocaleString()}</p>
                         <p className='box-details'>Shots Fired: {WeaponWithMostKills.TotalShotsFired.toLocaleString()}</p>
@@ -210,13 +246,12 @@ class Arenapage extends Component {
                     </div>
                     <div className='arena-details-box box-d'>
                       <h4 className='box-title'>Shooting Stats</h4>
-                      <p className='box-details'>Total Damage Done: {parseInt(TotalWeaponDamage).toLocaleString()}</p>
-                      <p className='box-details'>Shots Fired: {TotalShotsFired.toLocaleString()}</p>
-                      <p className='box-details'>Shots Landed: {TotalShotsLanded.toLocaleString()}</p>
-                      <p className='box-details'>Headshots: {TotalHeadshots.toLocaleString()}</p>
-                    </div>
-                    <div className='arena-details-box box-e'>
-                      <h4 className='box-title'>Impulse Stats</h4>
+                      <div>
+                        <p className='box-details'>Total Damage Done: {parseInt(TotalWeaponDamage).toLocaleString()}</p>
+                        <p className='box-details'>Shots Fired: {TotalShotsFired.toLocaleString()}</p>
+                        <p className='box-details'>Shots Landed: {TotalShotsLanded.toLocaleString()}</p>
+                        <p className='box-details'>Headshots: {TotalHeadshots.toLocaleString()}</p>
+                      </div>
                     </div>
                     <div className='arena-details-box box-f'>
                       <h4 className='box-title'>Top Medals</h4>
@@ -239,17 +274,9 @@ class Arenapage extends Component {
                         )
                       })}
                     </div>
-                    <div className='arena-details-box box-g'>
-                      <h4 className='box-title'>Flexible Stats</h4>
-                    </div>
                     <div className='arena-details-box box-h'>
-                      <h4 className='box-title'>Style Kills</h4>
-                      <p className='box-details'>Assassinations: {TotalAssassinations.toLocaleString()}</p>
-                      <p className='box-details'>Melee: {TotalMeleeKills.toLocaleString()}</p>
-                      <p className='box-details'>Ground Pound: {TotalGroundPoundKills.toLocaleString()}</p>
-                      <p className='box-details'>Shoulder Bash: {TotalShoulderBashKills.toLocaleString()}</p>
-                      <p className='box-details'>Grenade: {TotalGrenadeKills.toLocaleString()}</p>
-                      <p className='box-details'>Power Weapon: {TotalPowerWeaponKills.toLocaleString()}</p>
+                      <h4 className='box-title'>Highest Rank Attained</h4>
+                      
                     </div>
                     <div className='arena-details-box box-i'>
                       <img className='arena-spartan' src={this.props.currentImgUrlSpartan} alt='Users spartan' />
