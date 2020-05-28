c9b6c5dfdfbc74ee5cd3e66a5232f5ce6de68cf5
Jeremy Poulter <Jpoulter@gmail.com>
2020-05-28 16:14:23 -0600

    Create template slides and render carousel component

--- client/src/containers/Carousel/Carousel.js
+++ client/src/containers/Carousel/Carousel.js
@@ -1,8 +1,32 @@
 import React, { Component } from "react";
 import "./Carousel.scss";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Slider from 'react-slick';
+
 
 export default class Carousel extends Component {
+
   render() {
-    return <div>Carousel coming soon...</div>;
+    const renderSlides = () =>
+      [1, 2, 3, 4, 5, 6, 7, 8].map(num => (
+        <div className='carousel-slide'>
+          <h3>Slide {num}</h3>
+          <p className='emojis'>🔬 🎦 🤡 🏦 💰</p>
+          <p>********Sample Text For Display*******</p>
+        </div>
+      ))
+    return (
+      <div className='App'>
+        <Slider
+          dots={true}
+          slidesToShow={1}
+          slidesToScroll={1}
+          autoplay={true}
+          autoplaySpeed={2000}
+        >
+          {renderSlides()}
+        </Slider>
+      </div>)
   }
 }
