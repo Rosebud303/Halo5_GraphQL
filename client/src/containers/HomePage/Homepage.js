78e4acb81ce419c43b5339f23a5f785f3b5f47a5
Duy Vu <duyv.thanh@gmail.com>
2020-08-20 00:24:51 -0600

    Changed description message based on which button is hovered.
    Co-authored-by: Jeremy Poulter <jpoulter@gmail.com>
    Co-authored-by: Duy Vu <duyv.thanh@gmail.com>

--- client/src/containers/HomePage/Homepage.js
+++ client/src/containers/HomePage/Homepage.js
@@ -8,20 +8,21 @@ import { api_key, proxyurl } from '../../apikey';
 import axios from 'axios';
 import Spinner from '../../Spinner/Spinner';
 
+const descriptions =  [
+  'this is your profile',
+  'this is your details',
+  'this is your arena',
+  'this is your warzone',
+  'these are the metadata libraries'
+]
 
 class Homepage extends Component {
   constructor() {
     super();
     this.state = {
       searchedPlayer: '',
-      currentIndex: 0,
-      descriptions: [
-        'this is your profile',
-        'this is your details',
-        'this is your arena',
-        'this is your warzone',
-        'these are the metadata libraries'
-      ]
+      currentDescription: descriptions[0],
+
     };
   }
 
@@ -31,138 +32,140 @@ class Homepage extends Component {
   }
 
   handleMouseOver = (e) => {
-    this.setState({currentIndex: parseInt(e.target.id)})
+    this.setState({ currentDescription: descriptions[parseInt(e.target.id)] })
   }
 
-  handleMouseOut = () => {
-    this.setState({currentIndex: 0})
-  }
-
-  handleChange = (event) => {
-    this.setState({
-      searchedPlayer: event.target.value,
-    });
-  };
-
-  handleSubmit = async (event) => {
-    event.preventDefault();
-    this.props.setImgUrlSpartan('');
-    this.props.setImgUrlEmblem('');
-    await this.props.currentSearchedPlayer(this.state.searchedPlayer);
-    this.setUrlSpartan();
-    this.setUrlEmblem();
-    this.clearInputs();
-  };
-
-  setUrlSpartan = () => {
-    axios
-      .create({
-        headers: { 'Ocp-Apim-Subscription-Key': api_key },
-      })
-      .get(
-        proxyurl +
-        `https://www.haloapi.com/profile/h5/profiles/${this.props.currentPlayer}/spartan?size=512`
-      )
-      .then((data) => this.props.setImgUrlSpartan(data.headers['x-final-url']));
-  };
-
-  setUrlEmblem = () => {
-    axios
-      .create({
-        headers: { 'Ocp-Apim-Subscription-Key': api_key },
-      })
-      .get(
-        proxyurl +
-        `https://www.haloapi.com/profile/h5/profiles/${this.props.currentPlayer}/emblem?size=512`
-      )
-      .then((data) => this.props.setImgUrlEmblem(data.headers['x-final-url']));
-  };
-
-  clearInputs = () => {
-    this.setState({ searchedPlayer: '' });
-  }
-
-  render() {
-    const { handleSubmit, handleChange, state: { searchedPlayer }, props: { currentPlayer, currentImgUrlSpartan, currentImgUrlEmblem } } = this
-    const spartanImageStyle = {
-      backgroundImage: `url(${currentImgUrlSpartan})`,
-      backgroundPosition: `-90px -25px`,
-      backgroundSize: 'auto',
-      width: '200px',
-      height: '230px'
+    handleMouseOut = () => {
+      this.setState({ currentDescription: descriptions[0] })
     }
 
-    return (
-      <>
-        <div className='carousel-search-options'>
-          <Carousel />
-          <form className='welcome-form' onSubmit={handleSubmit}>
-            <input
-              className='welcome-search welcome-search-input'
-              name='search'
-              onChange={handleChange}
-              placeholder='Find New Spartan...'
-              required
-              type='text'
-              value={searchedPlayer}
-            />
-            <button
-              className='welcome-search welcome-search-button'
-              type='submit'
-            >
-              Submit
+
+
+    handleChange = (event) => {
+      this.setState({
+        searchedPlayer: event.target.value,
+      });
+    };
+
+    handleSubmit = async (event) => {
+      event.preventDefault();
+      this.props.setImgUrlSpartan('');
+      this.props.setImgUrlEmblem('');
+      await this.props.currentSearchedPlayer(this.state.searchedPlayer);
+      this.setUrlSpartan();
+      this.setUrlEmblem();
+      this.clearInputs();
+    };
+
+    setUrlSpartan = () => {
+      axios
+        .create({
+          headers: { 'Ocp-Apim-Subscription-Key': api_key },
+        })
+        .get(
+          proxyurl +
+          `https://www.haloapi.com/profile/h5/profiles/${this.props.currentPlayer}/spartan?size=512`
+        )
+        .then((data) => this.props.setImgUrlSpartan(data.headers['x-final-url']));
+    };
+
+    setUrlEmblem = () => {
+      axios
+        .create({
+          headers: { 'Ocp-Apim-Subscription-Key': api_key },
+        })
+        .get(
+          proxyurl +
+          `https://www.haloapi.com/profile/h5/profiles/${this.props.currentPlayer}/emblem?size=512`
+        )
+        .then((data) => this.props.setImgUrlEmblem(data.headers['x-final-url']));
+    };
+
+    clearInputs = () => {
+      this.setState({ searchedPlayer: '' });
+    }
+
+    render() {
+      const { handleSubmit, handleChange, state: { searchedPlayer }, props: { currentPlayer, currentImgUrlSpartan, currentImgUrlEmblem } } = this
+      const spartanImageStyle = {
+        backgroundImage: `url(${currentImgUrlSpartan})`,
+        backgroundPosition: `-90px -25px`,
+        backgroundSize: 'auto',
+        width: '200px',
+        height: '230px'
+      }
+
+      return (
+        <>
+          <div className='carousel-search-options'>
+            <Carousel />
+            <form className='welcome-form' onSubmit={handleSubmit}>
+              <input
+                className='welcome-search welcome-search-input'
+                name='search'
+                onChange={handleChange}
+                placeholder='Find New Spartan...'
+                required
+                type='text'
+                value={searchedPlayer}
+              />
+              <button
+                className='welcome-search welcome-search-button'
+                type='submit'
+              >
+                Submit
             </button>
-          </form>
-        </div>
-        <div className='lesser-spartan-details'>
-          <section className='spartan-gfx'>
-            <p className='id-card-name'>{currentPlayer}</p>
+            </form>
+          </div>
+          <div className='lesser-spartan-details'>
+            <section className='spartan-gfx'>
+              <p className='id-card-name'>{currentPlayer}</p>
 
-            <div className='id-card-images'>
-              <div className='id-image-container-1'>
-                {currentImgUrlSpartan ? (
+              <div className='id-card-images'>
+                <div className='id-image-container-1'>
+                  {currentImgUrlSpartan ? (
 
-                  <div className='spartan-image-container' style={spartanImageStyle}></div>
+                    <div className='spartan-image-container' style={spartanImageStyle}></div>
 
 
-                ) : (
-                    <Spinner name={'Spartan'} />
-                  )}
+                  ) : (
+                      <Spinner name={'Spartan'} />
+                    )}
+                </div>
+                <div className='id-image-container-2'>
+                  {currentImgUrlEmblem ? (
+                    <img
+                      alt='Player Emblem'
+                      className='lesser-emblem'
+                      src={currentImgUrlEmblem}
+                    />
+                  ) : (
+                      <Spinner name={'Spartan'} />
+                    )}
+                </div>
               </div>
-              <div className='id-image-container-2'>
-                {currentImgUrlEmblem ? (
-                  <img
-                    alt='Player Emblem'
-                    className='lesser-emblem'
-                    src={currentImgUrlEmblem}
-                  />
-                ) : (
-                    <Spinner name={'Spartan'} />
-                  )}
-              </div>
-            </div>
 
-          </section>
-          <section className='banner-company-links'>
-            <p>{this.state.currentIndex}</p>
-            <h3 className='lesser-company'>COMPANY PLACEHOLDER</h3>
-            <Link onMouseOver={(event) => this.handleMouseOver(event)} onMouseOut={() => this.handleMouseOut()} id='1' to='/details' className='homepage-links'>
-              <p onMouseOver={(event) => this.handleMouseOver(event)} onMouseOut={() => this.handleMouseOut()} id='1' className='detail-link details'>DETAILS PAGE</p>
-            </Link>
-            <Link onMouseOver={(event) => this.handleMouseOver(event)} onMouseOut={() => this.handleMouseOut()} id='2' to='/arena' className='homepage-links'>
-              <p onMouseOver={(event) => this.handleMouseOver(event)} onMouseOut={() => this.handleMouseOut()} id='2' className='detail-link arena-lesser'>ARENA PAGE</p>
-            </Link>
-            <Link onMouseOver={(event) => this.handleMouseOver(event)} onMouseOut={() => this.handleMouseOut()} id='3' to='/warzone' className='homepage-links'>
-              <p onMouseOver={(event) => this.handleMouseOver(event)} onMouseOut={() => this.handleMouseOut()} id='3' className='detail-link warzone-lesser'>WARZONE PAGE</p>
-            </Link>
-            <Link onMouseOver={(event) => this.handleMouseOver(event)} onMouseOut={() => this.handleMouseOut()} id='4' to='/libraries' className='homepage-links'>
-              <p onMouseOver={(event) => this.handleMouseOver(event)} onMouseOut={() => this.handleMouseOut()} id='4' className='detail-link warzone-lesser'>LIBRARIES PAGE</p>
-            </Link>
-          </section>
-        </div>
-      </>
-    );
-  }
+            </section>
+            <section className='banner-company-links'>
+              <p className='description'>{this.state.currentDescription}</p>
+              <h3 className='lesser-company'>COMPANY PLACEHOLDER</h3>
+              <Link onMouseOver={(event) => this.handleMouseOver(event)} onMouseOut={() => this.handleMouseOut()} id='1' to='/details' className='homepage-links'>
+                <p onMouseOver={(event) => this.handleMouseOver(event)} onMouseOut={() => this.handleMouseOut()} id='1' className='detail-link details'>DETAILS PAGE</p>
+              </Link>
+              <Link onMouseOver={(event) => this.handleMouseOver(event)} onMouseOut={() => this.handleMouseOut()} id='2' to='/arena' className='homepage-links'>
+                <p onMouseOver={(event) => this.handleMouseOver(event)} onMouseOut={() => this.handleMouseOut()} id='2' className='detail-link arena-lesser'>ARENA PAGE</p>
+              </Link>
+              <Link onMouseOver={(event) => this.handleMouseOver(event)} onMouseOut={() => this.handleMouseOut()} id='3' to='/warzone' className='homepage-links'>
+                <p onMouseOver={(event) => this.handleMouseOver(event)} onMouseOut={() => this.handleMouseOut()} id='3' className='detail-link warzone-lesser'>WARZONE PAGE</p>
+              </Link>
+              <Link onMouseOver={(event) => this.handleMouseOver(event)} onMouseOut={() => this.handleMouseOut()} id='4' to='/libraries' className='homepage-links'>
+                <p onMouseOver={(event) => this.handleMouseOver(event)} onMouseOut={() => this.handleMouseOut()} id='4' className='detail-link warzone-lesser'>LIBRARIES PAGE</p>
+              </Link>
+            </section>
+          </div>
+        </>
+      );
+    }
 }
 
 const mapStateToProps = (state) => ({
